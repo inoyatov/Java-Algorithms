3d0d8ad38eeb7758aba236e93c461a12c5b39c4d
Khamidulla Inoyatov <khamidulla@inoyatov.com>
2018-04-12 10:31:00 +0900

    JUnit and Hamcrast added

--- src/App.java
+++
@@ -1,7 +0,0 @@
-package software;
-
-class App {
-    public static void main(String[] args) {
-        System.out.println("Entry Point of App.");
-    }
-}
---
+++ src/com/inoyatov/automation/Application.java
@@ -0,0 +1,8 @@
+package com.inoyatov.automation;
+
+
+class Application {
+    public static void main(String[] args) {
+        System.out.println("Entry point of application");
+    }
+}
